908c8abec16fe0a5712c44bbdd80bbad1ee02614
bomi <bonita9931@gmail.com>
2021-03-07 23:44:27 +0900

    feat: 과거상세기록보기 페이지 카드 추가

--- src/pages/PastDetail.js
+++ src/pages/PastDetail.js
@@ -1,9 +1,11 @@
 import React from "react";
 import MainStructure from "../components/structure/MainStructure";
+import Detail from "../components/record/detail"
 
 
 const Past = () => {
     return <MainStructure>
+        <Detail />
         과거상세기록디테일페이지
     </MainStructure>;
 };
