48468c0e4669dbe337ab4b6c0b0f23a3a8798b5f
bomi <bonita9931@gmail.com>
2021-05-10 01:43:15 +0900

    feat: 라이브캠 전 기본 정보 입력받는 페이지 생성

--- src/pages/PrepareCam.js
+++ src/pages/PrepareCam.js
@@ -1,10 +1,12 @@
 import MainStructure from "../components/structure";
 import Auth from "../components/auth/AuthForm/AuthForm";
+import Cam from "../components/camera/prepareCam";
 import React from "react";
 
 const PrepareCam = () => {
     return <MainStructure>
         <Auth />
+        <Cam />
     </MainStructure>;
 };
 
