79f219638bf3438990d5d109b719df69106dfcd5
bomi <bonita9931@gmail.com>
2021-05-26 21:42:36 +0900

    .

--- scripts/start.js
+++ scripts/start.js
@@ -45,7 +45,7 @@ if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
 }
 
 // Tools like Cloud9 rely on this.
-const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 5004;
+const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 6061;
 const HOST = process.env.HOST || '0.0.0.0';
 
 // const DEFAULT_PORT = 8080;
